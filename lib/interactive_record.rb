e4c333bb385a0e576f4008c45cc480a52d7715ad
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-05-01 20:16:22 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,8 +1,10 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
+binding.pry
 class InteractiveRecord
 
+  binding.pry
   def table_name
     binding.pry
     self.to_s.doncase.plurilize
